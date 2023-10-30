53a97d1d6465672a5d884d394db8ce6017424cd2
Stelios Tzamarias <111519229+austrelios@users.noreply.github.com>
2023-10-30 20:54:57 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -44,8 +44,25 @@ function carousel() {
 
 
 
-// Code for Google MediaCapabilities
+// This is the code for the tabsheets
+// *************************************
+function openTabsheet(evt, tabsheetName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabsheetName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
+
+
+// Code for Google MediaCapabilities
 let map;
 
 async function initMap() {
